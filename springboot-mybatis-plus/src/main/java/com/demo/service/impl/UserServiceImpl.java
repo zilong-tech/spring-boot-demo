48c644262495ebb3efb161f131898b0168f9f966
javacode <zysspace@163.com>
2023-06-14 09:43:28 +0800

    集成mybatis-plus

---
+++ springboot-mybatis-plus/src/main/java/com/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,79 @@
+package com.demo.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.demo.domain.User;
+import com.demo.mapper.UserMapper;
+import com.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * Description:
+ * <p>
+ * Create on 2023/06/13
+ *
+ * @author zys
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public boolean saveBatch(Collection<User> entityList, int batchSize) {
+        return false;
+    }
+
+    @Override
+    public boolean saveOrUpdateBatch(Collection<User> entityList, int batchSize) {
+        return false;
+    }
+
+    @Override
+    public boolean updateBatchById(Collection<User> entityList, int batchSize) {
+        return false;
+    }
+
+    @Override
+    public boolean saveOrUpdate(User entity) {
+
+       return save(entity);
+    }
+
+    @Override
+    public User getOne(Wrapper<User> queryWrapper, boolean throwEx) {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getMap(Wrapper<User> queryWrapper) {
+        return null;
+    }
+
+    @Override
+    public <V> V getObj(Wrapper<User> queryWrapper, Function<? super Object, V> mapper) {
+        return null;
+    }
+
+    @Override
+    public BaseMapper<User> getBaseMapper() {
+        return this.userMapper;
+    }
+
+    @Override
+    public Class<User> getEntityClass() {
+        return null;
+    }
+
+    @Override
+    public void saveUser(User user) {
+        saveOrUpdate(user);
+    }
+}
